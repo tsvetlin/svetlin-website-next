fca53d4aeeab19a538d567419dcda6eceb7113f3
Szvetlin Tanyi <svetlintanyi@gmail.com>
2024-12-22 14:48:03 +0100

    Breakpoints

--- app/ui/About/AboutSection.tsx
+++ app/ui/About/AboutSection.tsx
@@ -5,26 +5,28 @@ import LongDescription from './LongDescription';
 
 export default function AboutSection() {
   return (
-    <section id="about" className="max-w-5xl m-auto">
-      <div className="flex flex-row mb-8">
-        <div>
-          <h1 className="font-bold text-7xl mb-12">
+    <section id="about" className="max-w-5xl m-auto sm:p-6 md:p-8 lg:p-0">
+      <div className="flex flex-col md:flex-row mb-8">
+        <div className="flex-1 mb-8 md:mb-0">
+          <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl mb-6 sm:mb-8 md:mb-12">
             Hi there! I&apos;m Szvetlin.
           </h1>
           <ShortDescription />
-          <div className="mr-8">
+          <div className="mt-4 sm:mt-6 md:mt-8">
             <SocialList />
           </div>
         </div>
 
-        <Image
-          className="rounded-lg hover:scale-110 transition-all duration-500"
-          src="/prof.jpeg"
-          alt="Szvetlin"
-          width={334}
-          height={400}
-          priority
-        />
+        <div className="flex justify-center md:justify-end">
+          <Image
+            className="rounded-lg hover:scale-110 transition-all duration-500"
+            src="/prof.jpeg"
+            alt="Szvetlin"
+            width={334}
+            height={400}
+            priority
+          />
+        </div>
       </div>
       <LongDescription />
     </section>
