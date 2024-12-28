0eaa54987412c0952b6b02b032668d7428b9ae30
Szvetlin Tanyi <svetlintanyi@gmail.com>
2024-12-28 17:07:39 +0100

    Name link to landing

--- app/ui/Header/Header.tsx
+++ app/ui/Header/Header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import List from './List';
 import ListItem from './ListItem';
 
@@ -5,7 +7,9 @@ export default function Header() {
   return (
     <header className="flex flex-col md:flex-row justify-between md:h-20 content-center items-center max-w-6xl m-auto mb-8 mt-8 md:mb-12 md:p-8 lg:p-0">
       <h1 className="content-center font-bold hidden md:block text-norwrap">
-        <em>Szvetlin Tanyi</em>
+        <Link href="/">
+          <em>Szvetlin Tanyi</em>
+        </Link>
       </h1>
       <nav className="flex sm:mt-4 md:mt-0 md:ml-4 w-full md:w-auto">
         <List>
